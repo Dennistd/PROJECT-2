ea3370de70ebbfbbfdd44cd1b79a2fbe1d48cd71
Dennis <xxyeezusxx@icloud.com>
2020-05-17 20:36:44 +0300

    Initial Commit

---
+++ PROJECT Hotev v3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PROJECT Hotev v3
+//
+//  Created by Dennis Dimitrov on 17.05.20.
+//  Copyright © 2020 Dennis Dimitrov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
